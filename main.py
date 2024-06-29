ecbab8bc81167f9fa43f9d2631ed4dd0fd0aa862
Hussein Termos <hat30@mail.aub.edu>
2024-06-30 00:23:20 +0300

    Add vehicle location functionalities - GET: to get the location of a vehicle knowing its id. - POST: to add the location of a new vehicle. - PUT: to update the location of an existing vehicle.
    Note: For now the app only runs from `http://127.0.0.1:8000` and
    not `http://localhost:8000`

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import asyncpg
 import os
 from dotenv import load_dotenv, find_dotenv
 
+from pydantic import BaseModel
+
+# For data validation
+class vehicle_location(BaseModel):
+    id: int
+    latitude: float
+    longitude: float
+
+
 # Get access credentials to database
 load_dotenv(find_dotenv())
 DB_URL = os.getenv("db_url")
@@ -20,6 +29,27 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/vehicle_location/{vehicle_id}")
+async def get_vehicle_location(vehicle_id: int):
+    entry = await app.state.db.fetchrow("""SELECT * FROM vehicle_location WHERE vehicle_id=$1
+                         """, vehicle_id)
+    
+    return {'longitude': entry["longitude"], "latitude" : entry["latitude"]}
+
+@app.post("/vehicle_location")
+async def post_vehicle_location(vehicle_location_data: vehicle_location):
+    vehicle_id, latitude, longitude = vehicle_location_data.id, vehicle_location_data.latitude, vehicle_location_data.longitude
+    await app.state.db.execute("""INSERT INTO vehicle_location 
+                               (vehicle_id, latitude, longitude) VALUES ($1, $2, $3)""", vehicle_id, latitude, longitude)
+
+
+@app.put("/vehicle_location")
+async def post_vehicle_location(vehicle_location_data: vehicle_location):
+    vehicle_id, latitude, longitude = vehicle_location_data.id, vehicle_location_data.latitude, vehicle_location_data.longitude
+    await app.state.db.execute("UPDATE vehicle_location SET longitude=$1, latitude=$2 WHERE vehicle_id=$3", longitude, latitude, vehicle_id)
+    
 
 # Automatically redirect requests for "/" to react
 app.mount("/", StaticFiles(directory="my-app/build", html=True), name="React-Project")
