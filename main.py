26719febc2a3963f1d3f9e6b060dbeaf3e3fd95e
Hussein Termos <hat30@mail.aub.edu>
2024-06-30 00:15:05 +0300

    Establish connection with database from fastapi

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
+
+from contextlib import asynccontextmanager
+import asyncpg
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Get access credentials to database
+load_dotenv(find_dotenv())
+DB_URL = os.getenv("db_url")
+
+# Open connection to database when app starts up
+# and close the connection when the app shuts down
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.db = await asyncpg.connect(DB_URL)
+    yield
+    await app.state.db.close()
+
+
+app = FastAPI(lifespan=lifespan)
 
 # Automatically redirect requests for "/" to react
 app.mount("/", StaticFiles(directory="my-app/build", html=True), name="React-Project")
