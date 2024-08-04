10c7ee96cd01a0a6a88ead8ebd65835290960625
Hussein Termos <hat30@mail.aub.edu>
2024-08-05 02:44:22 +0300

    Add feedback table for testing

--- create_db_tables.py
+++ create_db_tables.py
@@ -14,7 +14,9 @@ def recreate_tables():
 
     cur = conn.cursor()
 
-    cur.execute(""" DROP TABLE IF EXISTS passenger;
+    cur.execute(""" 
+                    DROP TABLE IF EXISTS feedback;
+                    DROP TABLE IF EXISTS passenger;
                     DROP TABLE IF EXISTS vehicle_location;
                     DROP TABLE IF EXISTS vehicle;
                     DROP TABLE IF EXISTS waypoint;
@@ -59,6 +61,23 @@ def recreate_tables():
                                            CONSTRAINT fk_route
                                                 FOREIGN KEY(route_id)
                                                     REFERENCES route(id)
+                                                );
+                    CREATE TABLE feedback  (id SERIAL PRIMARY KEY,
+                                            passenger_id INT NOT NULL,
+                                            vehicle_id INT NOT NULL,
+                                            true_false_example BOOLEAN,
+                                            text_example VARCHAR(255),
+                                            CONSTRAINT fk_passenger
+                                                FOREIGN KEY(passenger_id)
+                                                    REFERENCES passenger(id),
+                                            CONSTRAINT fk_vehicle
+                                                FOREIGN KEY(vehicle_id)
+                                                    REFERENCES vehicle(id),
+                                            CONSTRAINT not_empty
+                                                CHECK  ((true_false_example IS NOT NULL) OR
+                                                        (text_example IS NOT NULL)),
+                                            CONSTRAINT unique_feedback
+                                                UNIQUE(passenger_id, vehicle_id)
                                                 );
                 """)
 
